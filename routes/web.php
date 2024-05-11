ddd694f92abc261e691fd691f415e93a3aae53c4
AlexandroGonSan <alexandrogonsan@outlook.com>
2024-05-11 14:08:56 -0400

    implements test page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('welcome'); });
+Route::get('/tests', function () { return view('tests'); });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
