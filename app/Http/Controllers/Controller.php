e1c3dee70fcd94818f3fab044735d06b78583a57
AlexandroGonSan <alexandrogonsan@outlook.com>
2024-06-02 17:40:31 -0400

    debug controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,6 +14,7 @@ class Controller extends BaseController
 	public function LoadING(Request $request, $NextRoute = null): View
     {
         if ($NextRoute !== null) {
+            dd();
             return view('LoadING', ['NextRoute' => $NextRoute]);
         }
         dd();
