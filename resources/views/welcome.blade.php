6f22a6d33df301a47b09ae0952ccfb28e2fb04a5
AlexandroGonSan <alexandrogonsan@outlook.com>
2024-06-02 18:14:23 -0400

    ...debug

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,18 +69,18 @@
 							{{ __('Enter') }}
 						</button>
 					@endauth
-<button class="p-3 border-0 h" onclick="window.location.href = '/load0/1'" type="button">{{ __('Test') }}</button>
-<button class="p-3 border-0 h" onclick="window.location.href = '/load02/1'" type="button">{{ __('Test') }}</button>
-<button class="p-3 border-0 h" onclick="window.location.href = '/load1/1'" type="button">{{ __('Test') }}</button>
-<button class="p-3 border-0 h" onclick="window.location.href = '/load12/1'" type="button">{{ __('Test') }}</button>
-<button class="p-3 border-0 h" onclick="window.location.href = '/load2/1'" type="button">{{ __('Test') }}</button>
-<button class="p-3 border-0 h" onclick="window.location.href = '/load22/1'" type="button">{{ __('Test') }}</button>
-<button class="p-3 border-0 h" onclick="window.location.href = '/load3/1'" type="button">{{ __('Test') }}</button>
-<button class="p-3 border-0 h" onclick="window.location.href = '/load32/1'" type="button">{{ __('Test') }}</button>
-<button class="p-3 border-0 h" onclick="window.location.href = '/load4/1'" type="button">{{ __('Test') }}</button>
-<button class="p-3 border-0 h" onclick="window.location.href = '/load42/1'" type="button">{{ __('Test') }}</button>
-<button class="p-3 border-0 h" onclick="window.location.href = '/load5/1'" type="button">{{ __('Test') }}</button>
-<button class="p-3 border-0 h" onclick="window.location.href = '/load52/1'" type="button">{{ __('Test') }}</button>
+<a class="p-3 border-0 h" onclick="window.location.href = '/load0/1'" type="a">{{ __('Test') }}</a>
+<a class="p-3 border-0 h" onclick="window.location.href = '/load02/1'" type="a">{{ __('Test') }}</a>
+<a class="p-3 border-0 h" onclick="window.location.href = '/load1/1'" type="a">{{ __('Test') }}</a>
+<a class="p-3 border-0 h" onclick="window.location.href = '/load12/1'" type="a">{{ __('Test') }}</a>
+<a class="p-3 border-0 h" onclick="window.location.href = '/load2/1'" type="a">{{ __('Test') }}</a>
+<a class="p-3 border-0 h" onclick="window.location.href = '/load22/1'" type="a">{{ __('Test') }}</a>
+<a class="p-3 border-0 h" onclick="window.location.href = '/load3/1'" type="a">{{ __('Test') }}</a>
+<a class="p-3 border-0 h" onclick="window.location.href = '/load32/1'" type="a">{{ __('Test') }}</a>
+<a class="p-3 border-0 h" onclick="window.location.href = '/load4/1'" type="a">{{ __('Test') }}</a>
+<a class="p-3 border-0 h" onclick="window.location.href = '/load42/1'" type="a">{{ __('Test') }}</a>
+<a class="p-3 border-0 h" onclick="window.location.href = '/load5/1'" type="a">{{ __('Test') }}</a>
+<a class="p-3 border-0 h" onclick="window.location.href = '/load52/1'" type="a">{{ __('Test') }}</button>
 				</div>
 				<button class="dropdown-toggle p-3 border-0 h" type="button" data-bs-toggle="dropdown" aria-expanded="false">
 					<i class="bi bi-gear"></i>
