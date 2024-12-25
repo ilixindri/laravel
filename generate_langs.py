0e05b7768495b8115aaeeb18c63524a5027154c2
Alex <alexandrogonsan@outlook.com>
2024-12-25 05:17:39 -0400

    code of generate langs pronto

--- generate_langs.py
+++ generate_langs.py
@@ -1,7 +1,23 @@
+
+"""
+traduzir os arquivos default de langs
+
+pesquisar por regex __('texto') e popular os arquivos lang
+
+ao pegar o texto do regex verificar qual a lang
+pois vou começar a escrever em pt e fazer
+o replace por en caso precise
+por exemplo deixar publico o codigo
+
+primeiros 500k de caracteres por mes free
+"""
+
 import os; import json; import glob
 from google.cloud import translate
 from dotenv import load_dotenv
 from google.api_core.retry import Retry
+
+CARACTERES_TRADUZIDOS = 0
 
 def translate_text(target: str = '',
 	text: str = "YOUR_TEXT_TO_TRANSLATE"):
@@ -33,35 +49,51 @@ load_dotenv(dotenv_path=a1)
 
 a1 = os.path.abspath(os.path.join(os.path.dirname(__file__), 'supported_languages.json'))
 with open(a1, 'r', encoding='utf-8') as f: languages = json.loads(f.read())
+quantidade_de_lang = len(languages)
 
 ens = {}
 root = os.path.join(os.path.dirname(__file__), 'lang')
 for f in os.scandir(os.path.join(root, 'en')):
 	if f.is_file():
 		with open(f.path, 'r', encoding='utf-8') as file:
-			ens[os.path.basename(f.path)] = file.read().split('\n')
+			ens[f.name] = file.read()
 
-for lang in languages:
+for numero_lang, lang in enumerate(languages, start=1):
 	lang_code = lang['language_code']
 	lang_name = lang['display_name']
 	target = lang['support_target']
 	if target:
-		for f in os.scandir(root):
-			if f.is_dir():
-				for g in os.scandir(os.path.join(root, f.path)):
-					if g.is_file():
+		for subpasta in os.scandir(root):
+			print(subpasta.path)
+			if subpasta.is_dir():
+				if subpasta.name in ['en']:
+					continue
+				for file in os.scandir(os.path.join(root, subpasta.path)):
+					print(file.path)
+					if file.is_file():
+						if CARACTERES_TRADUZIDOS > 300000:
+							exit()
 						content = ''
-						with open(g.path, 'r', encoding='utf-8') as file:
-							for line in file:
-								if line not in ens[os.path.basename(g.path)]:
+						number = 0
+						with open(file.path, 'r', encoding='utf-8') as f:
+							number = len(f.read().split('\n'))
+						with open(file.path, 'r', encoding='utf-8') as f:
+							for numero_linha, line in enumerate(f, start=1):
+								print(f'{numero_linha}/{number} de {numero_lang}/{quantidade_de_lang}')
+								if line.strip() not in ens[g.name]:
+									content += line
 									continue
 								a99 = line.split('=>')
 								if len(a99) not in [1, 2]:
 									print(f'error, 01, {g.path}')
 									exit()
 								aux = line
-								if a99[-1] != '[' or len(a99) != 1:
+								if a99[-1] != '[' and len(a99) != 1:
+									print('Traduzindo:', a99[1].strip())
+									CARACTERES_TRADUZIDOS += len(a99[1])
 									aux = a99[0] + '=>' + translate_text(lang_code, a99[1])
 								content += aux
-						with open(g.path, 'w', encoding='utf-8') as file:
-							file.write(content)
+						with open(g.path, 'w', encoding='utf-8') as f:
+							f.write(content)
+							f.flush()
+							os.fsync(f.fileno())
--- populate_langs_folders_default.py
+++ populate_langs_folders_default.py
@@ -3,27 +3,57 @@
 """
 
 import os; import json; import shutil
+import time
 
 def copiar_pasta(origem, destino):
-    """Copia uma pasta inteira da origem para o destino.
+	"""Copia uma pasta inteira da origem para o destino.
 
-    Args:
-        origem: O caminho da pasta de origem.
-        destino: O caminho da pasta de destino.
-    """
-    try:
-        shutil.copytree(origem, destino)
-        print(f"Pasta copiada de '{origem}' para '{destino}' com sucesso.")
-    except FileExistsError:
-        print(f"Erro: A pasta de destino '{destino}' já existe.")
-    except Exception as e:
-        print(f"Ocorreu um erro durante a cópia: {e}")
+	Args:
+		origem: O caminho da pasta de origem.
+		destino: O caminho da pasta de destino.
+	"""
+	try:
+		shutil.copytree(origem, destino)
+		print(f"Pasta copiada de '{origem}' para '{destino}' com sucesso.")
+	except FileExistsError:
+		print(f"Erro: A pasta de destino '{destino}' já existe.")
+	except Exception as e:
+		print(f"Ocorreu um erro durante a cópia: {e}")
+
+def copiar_arquivos(pasta_origem, nova_pasta_destino):
+	os.makedirs(nova_pasta_destino, exist_ok=True)
+	for entrada in pasta_origem:
+			caminho_destino = os.path.join(nova_pasta_destino, entrada)
+			arquivo_copiado = False
+			tempo_espera = 0
+			
+			if os.path.basename(nova_pasta_destino) == 'en':
+				continue
+
+			while not arquivo_copiado:
+				try:
+					with open(caminho_destino, 'w', encoding='utf-8') as f:
+						f.write(pasta_origem[entrada])
+					arquivo_copiado = True
+				except PermissionError as e:
+					time.sleep(1)
+					print(f'{entrada} {nova_pasta_destino}')
+					tempo_espera += 1
+			print('.', end='')
 
 a1 = os.path.abspath(os.path.join(os.path.dirname(__file__), "supported_languages.json"))
 with open(a1, 'r', encoding='utf-8') as f: languages = json.loads(f.read())
 
+ens = {}
+root = os.path.join(os.path.dirname(__file__), 'lang')
+for f in os.scandir(os.path.join(root, 'en')):
+	if f.is_file():
+		with open(f.path, 'r', encoding='utf-8') as file:
+			ens[os.path.basename(f.path)] = file.read()
+
 for i in range(len(languages)):
 	lang_code = languages[i]['language_code']
 	target = languages[i]['support_target']
-	if target:
-		copiar_pasta(os.path.dirname(__file__) + '/en', os.path.dirname(__file__) + '/' + lang_code)
+	if target or lang_code != 'en':
+		# copiar_pasta(os.path.join(os.path.dirname(__file__), 'lang', 'en'), os.path.join(os.path.dirname(__file__), 'lang', lang_code))
+		copiar_arquivos(ens, os.path.join(os.path.dirname(__file__), 'lang', lang_code))
